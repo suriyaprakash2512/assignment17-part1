3c942475780a9966695351b10ec6934ce0cdb346
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-11-21 15:15:05 +0530

    assign17

---
+++ BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17_part1
+{
+    public class BankAccount
+    {
+        private static double nextAccountNumber = 123456789822;
+
+        public double AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+
+        private double balance;
+
+       public double Balance
+        {
+            get { return balance; }
+        }
+
+        public BankAccount(string accountHolderName)
+        {
+            AccountNumber = nextAccountNumber++;
+            AccountHolderName = accountHolderName;
+            balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: ${amount}. New balance: ${balance}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be greater than zero.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn: ${amount}. New balance: ${balance}");
+            }
+            else if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be greater than zero.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds for withdrawal.");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount("surya");
+
+            Console.WriteLine($"Account Number: {myAccount.AccountNumber}");
+            Console.WriteLine($"Account Holder: {myAccount.AccountHolderName}");
+            Console.WriteLine($"Initial Balance: {myAccount.Balance}");
+
+            myAccount.Deposit(10000);
+            myAccount.Withdraw(6700);
+
+            Console.WriteLine($"Final Balance: ${myAccount.Balance}");
+            Console.ReadKey();
+
+        }
+    }
+}
